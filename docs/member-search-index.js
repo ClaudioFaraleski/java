e90b7663b0e0f84136e168c65ada974df4f0f8eb
Claudio Faraleski <faraleski@hotmail.com>
2024-07-09 11:00:42 -0300

    doc

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"ParouImpar","l":"condicao(int)"},{"p":"<Unnamed>","c":"ParouImpar","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"ParouImpar","l":"ParouImpar()","u":"%3Cinit%3E()"}];updateSearchResults();
