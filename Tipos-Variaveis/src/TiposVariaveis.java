f38a262527d3ec1098358e0e3b0b5034ae433e59
Claudio Faraleski <faraleski@hotmail.com>
2024-07-09 07:47:29 -0300

    atualizado

--- Tipos-Variaveis/src/TiposVariaveis.java
+++ Tipos-Variaveis/src/TiposVariaveis.java
@@ -13,14 +13,14 @@ public class TiposVariaveis {
         boolean dispositivo = false;
         String nome = "Java";
 
-        System.out.println(num1 +" o tipo double e composto de 8 bytes Armazena números fracionários. Suficiente para armazenar 15 dígitos decimais");
-        System.out.println(num2 +" o tipo inteiro e composto de 4 bytes Armazena números inteiros de -2.147.483.648 a 2.147.483.647");
-        System.out.println(num3 +" o tipo byte e composto de 1 byte  Armazena números inteiros de -128 a 127");
-        System.out.println(num4 +" o tipo long e composto de 8 bytes Armazena números inteiros de -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807");
-        System.out.println(num5 +" o tipo short e composto 2 bytes Armazena números inteiros de -32.768 a 32.767");
-        System.out.println(num6 +" o tipo flot e composto de 4 bytes Armazena números fracionários. Suficiente para armazenar de 6 a 7 dígitos decimais");
-        System.out.println(sexo +" o tipo char e composto de 2 bytes Armazena um único caractere/letra ou valores ASCII");
-        System.out.println(dispositivo +" o tipo booleano  e composto de 1 bit Armazena valores verdadeiros ou falsos");
+        System.out.println(num1 +"\n o tipo double e composto de 8 bytes Armazena números fracionários. Suficiente para armazenar 15 dígitos decimais");
+        System.out.println(num2 +"\n o tipo inteiro e composto de 4 bytes Armazena números inteiros de -2.147.483.648 a 2.147.483.647");
+        System.out.println(num3 +"\n o tipo byte e composto de 1 byte  Armazena números inteiros de -128 a 127");
+        System.out.println(num4 +"\n o tipo long e composto de 8 bytes Armazena números inteiros de -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807");
+        System.out.println(num5 +"\n o tipo short e composto 2 bytes Armazena números inteiros de -32.768 a 32.767");
+        System.out.println(num6 +"\n o tipo flot e composto de 4 bytes Armazena números fracionários. Suficiente para armazenar de 6 a 7 dígitos decimais");
+        System.out.println(sexo +"\n o tipo char e composto de 2 bytes Armazena um único caractere/letra ou valores ASCII");
+        System.out.println(dispositivo +"\n o tipo booleano  e composto de 1 bit Armazena valores verdadeiros ou falsos");
         System.out.println(nome +" Uma variável String contém uma coleção de caracteres entre aspas duplas:");
         
         
