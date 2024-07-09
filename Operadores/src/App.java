f8e0a0284dabcea89899e520a4e07316fdcac972
Claudio Faraleski <faraleski@hotmail.com>
2024-07-09 08:04:37 -0300

    tipo de operadores

---
+++ Operadores/src/App.java
@@ -0,0 +1,27 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        double soma = 15.5 + 16.8;
+        int subtracao = 15 - 8;
+        int multiplicacao = 25 * 6;
+        int divisao = 85 / 8;
+        int modulo = 15 % 5;
+        double expressao = (34*8) + (85 /9);
+
+        System.out.println(" o resultado da soma é = " + soma );
+        System.out.println("\n");
+        System.out.println(" o resultado da subtração é = " + subtracao );
+        System.out.println("\n");
+        System.out.println("o resultado da Multiplicação é = " + multiplicacao );
+        System.out.println("\n");
+        System.out.println("o resultado da Divisao é = " + divisao );
+        System.out.println("\n");
+        System.out.println("o resultado do Modulo  é = " + modulo );
+        System.out.println("\n");
+        System.out.println("o resultado da expressão  é = " + expressao );
+        
+
+        
+
+    }
+}
